574fb5abf06787ab38f420e84132647da42a2458
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-07-26 21:02:26 -0300

    chore: added solid-ui + theme toggle

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,37 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { getCookie } from "vinxi/http";
+import { isServer } from "solid-js/web";
+import { ColorModeProvider, cookieStorageManagerSSR } from "@kobalte/core";
+import { ColorModeScript } from "@kobalte/core/src/index.jsx";
+import { ModeToggle } from "./components/ui/mode-toggle";
+
+function getServerCookies() {
+    "use server"
+
+    const colorMode = getCookie("kb-color-mode")
+    return colorMode ? `kb-color-mode="${colorMode}` : ``
+}
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+    const storageManager = cookieStorageManagerSSR(
+        isServer ? getServerCookies() : document.cookie
+    )
+
+    return (
+        <Router
+            root={props => (
+                <>
+                    <ColorModeScript storageType={storageManager.type} />
+                    <ColorModeProvider storageManager={storageManager}>
+                        <ModeToggle />
+                        <Suspense>{props.children}</Suspense>
+                    </ColorModeProvider>
+                </>
+            )}
+        >
+            <FileRoutes />
+        </Router>
+    );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,14 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function getNoteName(noteNumber: number): string {
     const names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
     const octave = Math.floor(noteNumber / 12) - 1
     const note = names[noteNumber % 12]
 
     return `${note}/${octave}`
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
